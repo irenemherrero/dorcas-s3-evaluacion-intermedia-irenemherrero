07f525bcb7dc5dd2f9a1f032239617ae56776f0d
irenemherrero <irenemherrero@gmail.com>
2018-07-27 17:03:59 +0200

    pokemon list

---
+++ src/PokemonCard.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import App from './App';
+
+class PokemonCard extends Component {
+    render() {
+        
+      return (
+        <div className="PokemonCard">
+          <img src={this.props.url} alt=""/>
+            <h1 className="PokemonName">{this.props.name}</h1>
+            <p className="Skill1"> {this.props.types[0]}</p>
+            <p className="Skill2"> {this.props.types[1]}</p>
+        </div>
+      );
+    }
+  }
+export default PokemonCard;
